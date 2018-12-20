0b576691200946e14eda7f5aa4df72b8a0c0d6e2
siempurahul <siempu.rahul31@gmail.com>
2018-12-20 20:52:29 +0530

    Added Bearish Harami Pattern

--- Modules/Patterns.R
+++ Modules/Patterns.R
@@ -2,3 +2,8 @@ Bearish_Engulfing<-function(Trade){
   if(!isS4(Trade)) return("Please check the input. It is not a Two Clandle StickClass")
   if(IsBullish(Trade@day1) & IsGapUp(Trade) & hasClosedBelow(Trade) & hasVolumeUp(Trade)) T else F
 }
+
+Bearish_Harami<-function(Trade){
+  if(!isS4(Trade)) return("Please check the input. It is not a Two Clandle StickClass")
+  if(IsBullish(Trade@day1) & IsGapDown(Trade) & hasClosedBelow(Trade) & hasVolumeDown(Trade)) T else F
+}
